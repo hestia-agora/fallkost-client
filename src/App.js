77e1b341a4add6005514a281c6e79ec5cc541138
Mihaela Maxim <maxim.mihaela1996@gmail.com>
2025-02-08 15:16:39 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Calculator from "./components/Calculator"; // Importera kalkylkomponenten
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+   
+      </header>
+      <main>
+        <Calculator /> {/* Rendera kalkylkomponenten */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
